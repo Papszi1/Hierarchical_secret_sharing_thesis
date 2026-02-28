c241e123c52d98a2b2b260400bbf14e6a3bc41bd
Pap Áron <paronka04@gmail.com>
2026-02-28 11:25:03 +0100

    created models.py where class Participant is defined

---
+++ models.py
@@ -0,0 +1,13 @@
+import os
+import numpy
+
+
+class Participant:
+    def __init__(self, id_i, level_j):
+        self.i = id_i
+        self.j = level_j
+        self.shares = []
+
+    def __repr__(self):
+        return f"P_{self.i}{self.j} (Level {self.j})"
+    
